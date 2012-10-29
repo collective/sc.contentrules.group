082472ed40041a7434180836e189826735d5d51a
Érico Andrei <erico@simplesconsultoria.com.br>
2012-10-29 14:52:23 -0200

    Update i18n script

--- i18n.sh
+++ i18n.sh
@@ -3,9 +3,12 @@
 # ensure that when something is wrong, nothing is broken more than it should...
 set -e
 
+BASEDIR=sc/contentrules/group
+LOCALES=$BASEDIR/locales
+
 # first, create some pot containing anything
-i18ndude rebuild-pot --pot sc.contentrules.group.pot --create sc.contentrules.group --merge manual.pot ..
+i18ndude rebuild-pot --pot $LOCALES/sc.contentrules.group.pot --create sc.contentrules.group --merge $LOCALES/manual.pot $BASEDIR
 
 # finally, update the po files
-i18ndude sync --pot sc.contentrules.group.pot  `find . -iregex '.*sc.contentrules.group\.po$'|grep -v plone`
+i18ndude sync --pot $LOCALES/sc.contentrules.group.pot  `find . -iregex '.*sc.contentrules.group\.po$'|grep -v plone`
 
