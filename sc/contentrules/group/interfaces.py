5a312ab63d29bb37c264951f8007b533dbe3bf8a
Érico Andrei <erico@simplesconsultoria.com.br>
2012-10-29 12:28:13 -0200

    Adding spaces to user messages

--- sc/contentrules/group/interfaces.py
+++ sc/contentrules/group/interfaces.py
@@ -14,19 +14,19 @@ class IGroupAction(Interface):
     groupid = TextLine(title=_(u"Group Id"),
                        description=_(u"Please inform the id for the user"
                                      u"group to be created by this action."
-                                     u"Use  ${title} in this field to use "
+                                     u" Use  ${title} in this field to use "
                                      u"the content title in the Group Id."),
                        required=True)
 
     grouptitle = TextLine(title=_(u"Group title"),
                           description=_(u"Please inform the title for the user"
                                         u" group to be created by this action."
-                                        u"Use ${title} in here to have the "
+                                        u" Use ${title} in here to have the "
                                         u"content title in the Group Title."),
                           required=False)
 
     roles = Set(title=_(u"Roles"),
-                description=_(u"Global roles to be assigned to the"
+                description=_(u"Global roles to be assigned to the "
                               u"user group created by this action."),
                 required=True,
                 value_type=Choice(vocabulary='plone.app.vocabularies.Roles'))
